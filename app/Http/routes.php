045f3b7f2df204a3cb9a2f67e968409eab56ad53
haqisaurus <haqisaurus@gmai.com>
2015-07-27 17:47:34 +0700

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,49 @@
 |
 */
 
+
+App::singleton('oauth2', function() {
+	
+	$storage = new OAuth2\Storage\Pdo(array('dsn' => 'mysql:dbname=webapi;host=localhost', 'username' => 'janganroot', 'password' => 'letmein!'));
+	$server = new OAuth2\Server($storage);
+	
+	$server->addGrantType(new OAuth2\GrantType\ClientCredentials($storage));
+	$server->addGrantType(new OAuth2\GrantType\UserCredentials($storage));
+	
+	return $server;
+});
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('oauth/token', function()
+{
+	$bridgedRequest  = OAuth2\HttpFoundationBridge\Request::createFromRequest(Request::instance());
+	$bridgedResponse = new OAuth2\HttpFoundationBridge\Response();
+	
+	$bridgedResponse = App::make('oauth2')->handleTokenRequest($bridgedRequest, $bridgedResponse);
+	
+	return $bridgedResponse;
+});
+Route::get('private', function()
+{
+	$bridgedRequest  = OAuth2\HttpFoundationBridge\Request::createFromRequest(Request::instance());
+	$bridgedResponse = new OAuth2\HttpFoundationBridge\Response();
+	
+	if (App::make('oauth2')->verifyResourceRequest($bridgedRequest, $bridgedResponse)) {
+		
+		$token = App::make('oauth2')->getAccessTokenData($bridgedRequest);
+		
+		return Response::json(array(
+			'private' => 'stuff',
+			'user_id' => $token['user_id'],
+			'client'  => $token['client_id'],
+			'expires' => $token['expires'],
+		));
+	}
+	else {
+		return Response::json(array(
+			'error' => 'Unauthorized'
+		), $bridgedResponse->getStatusCode());
+	}
+});
+
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,33 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         // $this->call(UserTableSeeder::class);
-
+        $this->call('OAuthClientsSeeder');
+        $this->call('OAuthUsersSeeder');
         Model::reguard();
+    }
+
+}
+
+class OAuthClientsSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('oauth_clients')->insert(array(
+            'client_id' => "testclient",
+            'client_secret' => "testpass",
+            'redirect_uri' => "http://fake/",
+        ));
+    }
+}
+class OAuthUsersSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('oauth_users')->insert(array(
+            'username' => "bshaffer",
+            'password' => sha1("brent123"),
+            'first_name' => "Brent",
+            'last_name' => "Shaffer",
+        ));
     }
 }
